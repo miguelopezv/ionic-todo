515b9780a5fc9d65859986ce097ae5338a69a581
Miguel López <miguellopez@vitalware.com>
2019-06-26 22:55:44 -0500

    update tab1 with service

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+import { List } from '../models/list.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  lists: List[];
+  constructor(public todoService: TodoService, private router: Router) {}
 
-  constructor() {}
-
+  addList() {
+    this.router.navigateByUrl('/tabs/tab1/add');
+  }
 }
