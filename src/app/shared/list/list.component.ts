70f5681a44465baee395e6518091386256646ca2
Miguel López <miguellopez@vitalware.com>
2019-06-30 18:51:11 -0500

    add shared module and list component

---
+++ src/app/shared/list/list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { List } from 'src/app/models/list.model';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent {
+  @Input() lists: List[];
+  @Output() goTo = new EventEmitter();
+
+  onClick(id: number) {
+    this.goTo.emit(id);
+  }
+}
